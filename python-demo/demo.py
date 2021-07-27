fb56e2c497b33682f59ef05e58363cbc737c5696
Qiu-INJ <kaitao@injectiveprotocol.com>
2021-07-27 17:26:10 +0800

    Backup.

--- python-demo/demo.py
+++ python-demo/demo.py
@@ -32,13 +32,18 @@ sys.path.insert(0, EXCHANGE_API_PATH)
 
 print("exchange api directory:{}".format(EXCHANGE_API_PATH))
 
-import injective_exchange_rpc_pb2 as exchange_rpc_pb
-import injective_exchange_rpc_pb2_grpc as exchange_rpc_grpc
-import injective_spot_exchange_rpc_pb2 as spot_exchange_rpc_pb
+# injective_spot_exchange_rpc.SpotLimitOrder
+
+import injective_accounts_rpc_pb2_grpc as accounts_rpc_grpc
+import injective_accounts_rpc_pb2 as accounts_rpc_pb
 import injective_spot_exchange_rpc_pb2_grpc as spot_exchange_rpc_grpc
+import injective_spot_exchange_rpc_pb2 as spot_exchange_rpc_pb
+import injective_exchange_rpc_pb2_grpc as exchange_rpc_grpc
+import injective_exchange_rpc_pb2 as exchange_rpc_pb
 
 class MarketMaker(object):
-    def __init__(self, spot_symbol: str, spot_market_id=None):
+    def __init__(self, account_id: str, spot_symbol: str, spot_market_id=None):
+        self.acct_id = account_id
         self.spot_symbol = spot_symbol + '_SPOT'
         if spot_market_id is not None:
             self.spot_market_id = spot_market_id
@@ -49,38 +54,67 @@ class MarketMaker(object):
                 if e.__cause__:
                     print('Cause:{}'.format(e.__cause__))
                 raise RuntimeError from e
-        print("symbol:{}, market_id:{}".format(self.spot_symbol, self.spot_market_id))
-        
+        print("symbol:{}, market_id:{}".format(
+            self.spot_symbol, self.spot_market_id))
 
-    async def get_trading_request(self) -> None:
+    async def get_trading_request(self) -> spot_exchange_rpc_pb.TradesResponse:
         async with grpc.aio.insecure_channel('localhost:9910') as channel:
-            exchange_rpc = exchange_rpc_grpc.InjectiveExchangeRPCStub(channel)
             spot_exchange_rpc = spot_exchange_rpc_grpc.InjectiveSpotExchangeRPCStub(
                 channel)
 
-            resp = await exchange_rpc.Version(exchange_rpc_pb.VersionRequest())
-            print("-- Connected to Injective Exchange (version: %s, built %s)" %
-                (resp.version, resp.meta_data["BuildDate"]))
-
-            resp = await spot_exchange_rpc.Markets(spot_exchange_rpc_pb.MarketsRequest())
-            print("\n-- Available markets:")
-            for m in resp.markets:
-                print(m.ticker, "=", m.market_id)
-
-            selected_market = resp.markets[0]
-
             mresp = await spot_exchange_rpc.Trades(spot_exchange_rpc_pb.TradesRequest(market_id=self.spot_market_id))
-            print("\n\033[1;34;40m API Response  \n")
-            print("\033[0;37;40m\n-- Order Update:", mresp)
-            # print(mresp.trades)
+            print("---Trade history of {}---".format(self.spot_symbol))
+            print(type(mresp))
             for trade in mresp.trades:
-                print("timestamp:{}\tdirection:{}\tprice:{} quantity:{}\tfee:{}\texecution_type:{}".format(\
-                    trade.price.timestamp, trade.trade_direction, trade.price.price, trade.price.quantity,\
+                print("timestamp:{}\tdirection:{}\tprice:{} quantity:{}\tfee:{}\texecution_type:{}".format(
+                    trade.price.timestamp, trade.trade_direction, trade.price.price, trade.price.quantity,
                     trade.fee, trade.trade_execution_type))
+            print("-----------------------------------")
+            return mresp
 
-    # async def get_market_request(self) ->None:
+    async def get_tick_info(self) -> spot_exchange_rpc_pb.MarketRequest:
+        async with grpc.aio.insecure_channel('localhost:9910') as channel:
+            spot_exchange_rpc = spot_exchange_rpc_grpc.InjectiveSpotExchangeRPCStub(
+                channel)
+            mresp = await spot_exchange_rpc.Market(spot_exchange_rpc_pb.MarketRequest(market_id=self.spot_market_id))
+            print(mresp)
+            return mresp
+
+    async def account_steam(self):
+        async with grpc.aio.insecure_channel('localhost:9910') as channel:
+
+            # accounts_rpc = accounts_rpc_grpc.InjectiveAccountsRPCStub(channel)
+            accounts_exchange_rpc = accounts_rpc_grpc.InjectiveAccountsRPCStub(
+                channel)
+
+            # # my account
+            # acct_id = "0xbdAEdEc95d563Fb05240d6e01821008454c24C36000000000000000000000000"
+            # # default account
+            # # acct_id = "0xaf79152ac5df276d9a8e1e2e22822f9713474902000000000000000000000000"
+            ord_direction = "buy"
+            sub_balance_stream = accounts_exchange_rpc.StreamSubaccountBalance(
+                accounts_rpc_pb.SubaccountBalance(subaccount_id=self.acct_id))
+
+            async for sub_balance in sub_balance_stream:
+                # acc = await accounts_exchange_rpc.SubaccountOrderSummary(accounts_rpc_pb.SubaccountOrderSummaryRequest(subaccount_id=acct_id, order_direction=ord_direction))
+                print("\n\033[1;34;40m API Response  \n")
+                print("\033[0;37;40m\n-- Order Update:", sub_balance)
+
+    async def buy(self, price, quantity):
+        async with grpc.aio.insecure_channel('localhost:9910') as channel:
+            spot_exchange_rpc = spot_exchange_rpc_grpc.InjectiveSpotExchangeRPCStub(
+                channel)
+            order_request = spot_exchange_rpc_pb.SpotLimitOrder(
+                order_side='buy', market_id=self.market_id, subaccount_id=self.acct_id, price=price, quantity=quantity)
+            order_return = spot_exchange_rpc.Order(order_request)
+            print(order_return)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    mm = MarketMaker('INJUSDT')
-    asyncio.get_event_loop().run_until_complete(mm.get_trading_request())
+    acct_id = "0xbdAEdEc95d563Fb05240d6e01821008454c24C36000000000000000000000000"
+    mm = MarketMaker(acct_id, 'INJUSDT')
+    # asyncio.get_event_loop().run_until_complete(mm.get_trading_request())
+    # asyncio.get_event_loop().run_until_complete(mm.get_tick_info())
+    # asyncio.get_event_loop().run_until_complete(mm.account_steam())
+    asyncio.get_event_loop().run_until_complete(mm.buy(0.1, 100))
