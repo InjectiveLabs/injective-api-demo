af08baa2b6dfc5958422e1b50144ca4e1636f292
Achilleas Kalantzis <kalantzis.achilleas@gmail.com>
2022-01-31 13:03:53 +0200

    chore: import dependencies from injective pkg

--- python-demo/strategy/cross_exchange_market_making/start.py
+++ python-demo/strategy/cross_exchange_market_making/start.py
@@ -1,5 +1,7 @@
 import os
 from configparser import ConfigParser
+import pyinjective
+import importlib.resources as pkg_resources
 from cross_exchange_market_making_batch import run_cross_exchange_market_making
 
 _current_dir = os.path.abspath(__file__)
@@ -18,14 +20,9 @@ if __name__ == "__main__":
     # read strategy configs
     cross_exchange_market_making_config = config["cross_exchange_market_making"]
 
-    ini_config_dir = os.path.join(
-        os.path.join(
-            os.path.dirname(os.path.dirname(os.path.dirname(_current_dir))), "config"
-        ),
-        "denoms_mainnet.ini",
-    )
+    ini_config_dir = denoms_mainnet = pkg_resources.read_text(pyinjective, 'denoms_mainnet.ini')
     ini_config = ConfigParser()
     # read denoms configs
-    ini_config.read(ini_config_dir)
+    ini_config.read_string(ini_config_dir)
 
     run_cross_exchange_market_making(cross_exchange_market_making_config, ini_config)
